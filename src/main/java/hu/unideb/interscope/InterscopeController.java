7b96be0d99d010bb9277a0d8e9d0c7f46e7a5ded
Ribar Krisztian <rybarkristik@gmail.com>
2024-10-25 12:57:13 +0900

    feat: Add animations to buttons, add missing logos, and improve button design

--- src/main/java/hu/unideb/interscope/InterscopeController.java
+++ src/main/java/hu/unideb/interscope/InterscopeController.java
@@ -1,55 +1,145 @@
 package hu.unideb.interscope;
 
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
+import javafx.animation.ScaleTransition;
+import javafx.scene.Node;
 
 public class InterscopeController {
 
-    public Text interscopeText;
+    @FXML
+    public Button emailButton;
+    @FXML
+    private GridPane userSearchGrid;
+    @FXML
+    public GridPane locationSearchGrid;
+    @FXML
+    private GridPane mainGrid;
 
     @FXML
     private ImageView appLogo;
     @FXML
-    private ImageView userSearchLogo;
+    private ImageView locationLogo;
+    @FXML
+    private ImageView emailLogo;
+    @FXML
+    private ImageView logsLogo;
+    @FXML
+    private ImageView userLogo;
+    @FXML
+    private ImageView preferencesLogo;
     @FXML
     private ImageView exitLogo;
-    @FXML
-    private Button userSearch;
-    @FXML
-    private Button button2;
-    @FXML
-    private Button button3;
-    @FXML
-    private Button button4;
-    @FXML
-    private Button button5;
-    @FXML
-    private Button exitButton;
-
 
 
     @FXML
-    private Text title;
+    public Text title;
     @FXML
     private Text description;
 
     @FXML
     public void initialize() {
-
         appLogo.setImage(new Image("/MenuLogo.png"));
+        userLogo.setImage(new Image("/userLogo.png"));
+        locationLogo.setImage(new Image("/locationLogo.png"));
+        emailLogo.setImage(new Image("/emailLogo.png"));
+        logsLogo.setImage(new Image("/logsLogo.png"));
+        preferencesLogo.setImage(new Image("/preferencesLogo.png"));
         exitLogo.setImage(new Image("exitLogo.png"));
-        userSearchLogo.setImage(new Image("/userSearchLogo.png"));
+
+
         Font.loadFont(getClass().getResourceAsStream("/fonts/JuliusSansOne.ttf"), 18);
-        exitButton.setGraphic(exitLogo);
     }
 
     @FXML
-    public void closeApplication(){
-            System.exit(0);
+    public void handleMenuButtonClick(ActionEvent event) {
+        swapGrids(userSearchGrid);
+        title.setText("Username Search");
+        description.setText("The username search looks across a lot of famous websites. please be aware that the searches are conducted using your machine's resources, and from your IP address, so search accordingly.");
+    }
+
+    @FXML
+    public void handleLocationButtonClick(ActionEvent event) {
+        swapGrids(locationSearchGrid);
+        title.setText("Location Search");
+    }
+
+    @FXML
+    public void handleLogsButtonClick(ActionEvent event) {
+        //TODO
+    }
+
+    @FXML
+    public void handleLogoHover(MouseEvent event) {
+        Node source = (Node) event.getSource();
+        ImageView imageView = null;
+
+        if (source instanceof Button button) {
+            if (button.getGraphic() instanceof VBox vbox) {
+                for (Node node : vbox.getChildren()) {
+                    if (node instanceof ImageView) {
+                        imageView = (ImageView) node;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (imageView == null) {
+            return;
+        }
+
+        ScaleTransition scaleUp = new ScaleTransition(Duration.millis(200), imageView);
+        scaleUp.setToX(1.3);
+        scaleUp.setToY(1.3);
+
+        ScaleTransition scaleDown = new ScaleTransition(Duration.millis(200), imageView);
+        scaleDown.setToX(1.0);
+        scaleDown.setToY(1.0);
+
+        // On Mouse Entered: Scale Up
+        if (event.getEventType() == MouseEvent.MOUSE_ENTERED) {
+            scaleUp.play();
+        }
+        // On Mouse Exited: Scale Down
+        else if (event.getEventType() == MouseEvent.MOUSE_EXITED) {
+            scaleDown.play();
+        }
+    }
+
+    private void swapGrids(GridPane grid) {
+        grid.setVisible(true);
+        grid.setTranslateX(mainGrid.getTranslateX() - 100);
+        grid.setTranslateY(-grid.getPrefHeight());
+        slideGridPaneOffScreen(mainGrid);
+        slideGridIn(grid);
+
+    }
+
+    private void slideGridPaneOffScreen(GridPane gridPane) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), gridPane);
+        transition.setByY(660);
+        transition.play();
+    }
+
+    private void slideGridIn(GridPane gridPane) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), gridPane);
+        transition.setToY(mainGrid.getTranslateY() - 10);
+        transition.play();
+    }
+
+    @FXML
+    public void closeApplication() {
+        System.exit(0);
     }
 }
-
