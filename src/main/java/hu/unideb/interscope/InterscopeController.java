ea32bc920d94fad02c2738b080794bb8e3645000
Ribar Krisztian <rybarkristik@gmail.com>
2024-10-26 23:47:44 +0900

    refactor: Improve InterscopeController by cleaning up code, centralizing grid swapping logic, and initializing images in FXML

--- src/main/java/hu/unideb/interscope/InterscopeController.java
+++ src/main/java/hu/unideb/interscope/InterscopeController.java
@@ -5,7 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -18,56 +17,30 @@ import javafx.scene.Node;
 
 public class InterscopeController {
     private enum AppPart {
-        MAINMENU, SIDEMENU
+        MAIN_MENU, SIDE_MENU
     }
 
-    @FXML
-    public Button backButton;
+    private @FXML Button userSearchButton;
+    private @FXML Button locationSearchButton;
+    private @FXML Button emailSearchButton;
+    private @FXML Button logsButton;
+    private @FXML Button backButton;
+    private @FXML Button settingsButton;
+    private @FXML Button exitButton;
 
-    @FXML
-    private GridPane mainGrid;
-    @FXML
-    private GridPane userSearchGrid;
-    @FXML
-    public GridPane locationSearchGrid;
-    @FXML
-    public GridPane emailSearchGrid;
-    @FXML
-    public GridPane logsGrid;
-    @FXML
-    public GridPane preferencesGrid;
+    private @FXML GridPane mainGrid;
+    private @FXML GridPane userSearchGrid;
+    private @FXML GridPane locationSearchGrid;
+    private @FXML GridPane emailSearchGrid;
+    private @FXML GridPane logsGrid;
+    private @FXML GridPane preferencesGrid;
+    private GridPane activeGrid;
 
-    public GridPane activeGrid;
-
-    @FXML
-    private ImageView appLogo;
-    @FXML
-    private ImageView locationLogo;
-    @FXML
-    private ImageView emailLogo;
-    @FXML
-    private ImageView logsLogo;
-    @FXML
-    private ImageView userLogo;
-    @FXML
-    private ImageView preferencesLogo;
-    @FXML
-    private ImageView exitLogo;
-
-    @FXML
-    public Label title;
-    @FXML
-    private Text description;
+    private @FXML Label title;
+    private @FXML Text description;
 
     @FXML
     public void initialize() {
-        appLogo.setImage(new Image("/MenuLogo.png"));
-        userLogo.setImage(new Image("/userLogo.png"));
-        locationLogo.setImage(new Image("/locationLogo.png"));
-        emailLogo.setImage(new Image("/emailLogo.png"));
-        logsLogo.setImage(new Image("/logsLogo.png"));
-        preferencesLogo.setImage(new Image("/preferencesLogo.png"));
-        exitLogo.setImage(new Image("exitLogo.png"));
         backButton.setTranslateY(100);
         backButton.setTranslateX(mainGrid.getWidth() - backButton.getWidth() - 20);
         activeGrid = mainGrid;
@@ -76,43 +49,36 @@ public class InterscopeController {
 
     @FXML
     public void handleMenuButtonClick(ActionEvent event) {
-        swapGrids(userSearchGrid, AppPart.MAINMENU);
-        title.setText("Username\nSearch");
-        description.setText("The username search looks across a lot of famous websites. please be aware that the searches " +
-                "are conducted using your machine's resources, and from your IP address, so search accordingly.");
-    }
-
-    @FXML
-    public void handleLocationButtonClick() {
-        swapGrids(locationSearchGrid, AppPart.MAINMENU);
-        title.setText("Location\nSearch");
-        description.setText("The Location search uses AI to analyze an image uploaded to the application and attempts to approximate where the photo is taken." +
-                "In the absence of GPS EXIF data, the system provides GPS estimation.");
-    }
-
-    @FXML
-    public void handleEmailButtonClick() {
-        swapGrids(emailSearchGrid, AppPart.MAINMENU);
-        title.setText("Email Search");
-        description.setText("The Domain Search returns all the email addresses found using one given domain name, with sources.\n" +
-                "The Email Finder finds the most likely email address from a domain name, a first name and a last name.");
-    }
-
-    @FXML
-    public void handleLogButtonClick() {
-        swapGrids(logsGrid, AppPart.MAINMENU);
-        title.setText("Previous\nSearches");
-        description.setText("Here you can find your previous searches. Maybe i will also implement a way to" +
-                "re-do the searches with the same parameters, although its unlikely.");
-    }
-    @FXML
-    public void handlePreferencesButtonClick() {
-        swapGrids(preferencesGrid, AppPart.MAINMENU);
+        if (event.getSource() == userSearchButton) {
+            swapGrids(userSearchGrid, AppPart.MAIN_MENU);
+            title.setText("Username\nSearch");
+            description.setText("The username search looks across a lot of famous websites. please be aware that the searches " +
+                    "are conducted using your machine's resources, and from your IP address, so search accordingly.");
+        } else if (event.getSource() == locationSearchButton) {
+            swapGrids(locationSearchGrid, AppPart.MAIN_MENU);
+            title.setText("Location\nSearch");
+            description.setText("The Location search uses AI to analyze an image uploaded to the application and attempts to approximate where the photo is taken." +
+                    "In the absence of GPS EXIF data, the system provides GPS estimation.");
+        } else if (event.getSource() == emailSearchButton) {
+            swapGrids(emailSearchGrid, AppPart.MAIN_MENU);
+            title.setText("Email Search");
+            description.setText("The Domain Search returns all the email addresses found using one given domain name, with sources.\n" +
+                    "The Email Finder finds the most likely email address from a domain name, a first name and a last name.");
+        } else if (event.getSource() == logsButton) {
+            swapGrids(logsGrid, AppPart.MAIN_MENU);
+            title.setText("Previous\nSearches");
+            description.setText("Here you can find your previous searches. Maybe i will also implement a way to" +
+                    "re-do the searches with the same parameters, although its unlikely.");
+        } else if (event.getSource() == settingsButton) {
+            swapGrids(preferencesGrid, AppPart.MAIN_MENU);
+        } else if (event.getSource() == exitButton) {
+            System.exit(0);
+        }
     }
 
     @FXML
     public void handleBackButtonClick() {
-        swapGrids(activeGrid, AppPart.SIDEMENU);
+        swapGrids(activeGrid, AppPart.SIDE_MENU);
         title.setText("InterScope");
         description.setText("Welcome to InterScope! InterScope is an application that consolidates the functionality of OSINT" +
                 " tools into a single, user-friendly platform, making it accessible without prior expertise. Developed by Ribár Krisztián " +
@@ -121,15 +87,13 @@ public class InterscopeController {
 
     @FXML
     public void handleLogoHover(MouseEvent event) {
-        Node source = (Node) event.getSource();
+        Button source = (Button) event.getSource();
         ImageView imageView = null;
-        if (source instanceof Button button) {
-            if (button.getGraphic() instanceof VBox vbox) {
-                for (Node node : vbox.getChildren()) {
-                    if (node instanceof ImageView) {
-                        imageView = (ImageView) node;
-                        break;
-                    }
+        if (source.getGraphic() instanceof VBox vbox) {
+            for (Node node : vbox.getChildren()) {
+                if (node instanceof ImageView) {
+                    imageView = (ImageView) node;
+                    break;
                 }
             }
         }
@@ -152,57 +116,37 @@ public class InterscopeController {
     }
 
     private void swapGrids(GridPane grid, AppPart appPart) {
-        if (appPart == AppPart.MAINMENU) {
-            slideMainGridOut(mainGrid);
-            slideSideGridIn(grid);
-            slideBackButtonUp();
+        if (appPart == AppPart.MAIN_MENU) {
+            slideMainGrid(true);
+            slideSideGrid(grid, false);
+            slideBackButton(false);
             activeGrid = grid;
-        } else if (appPart == AppPart.SIDEMENU) {
-            slideSideGridOut(grid);
-            slideMainGridIn(mainGrid);
-            slideBackButtonDown();
+        } else if (appPart == AppPart.SIDE_MENU) {
+            slideSideGrid(grid, true);
+            slideMainGrid(false);
+            slideBackButton(true);
             activeGrid = mainGrid;
         }
     }
 
-    private void slideMainGridOut(GridPane gridPane) {
-        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), gridPane);
-        transition.setByY(660);
+    private void slideMainGrid(boolean slideOut) {
+        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), mainGrid);
+        if (slideOut) transition.setByY(660);
+        else transition.setByY(-660);
         transition.play();
     }
 
-    private void slideMainGridIn(GridPane gridPane) {
+    private void slideSideGrid(GridPane gridPane, boolean slideOut) {
         TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), gridPane);
-        transition.setByY(-660);
+        if (slideOut) transition.setToY(-(mainGrid.getTranslateY()));
+        else transition.setToY(mainGrid.getTranslateY());
         transition.play();
     }
 
-    private void slideSideGridIn(GridPane gridPane) {
-        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), gridPane);
-        transition.setToY(mainGrid.getTranslateY());
-        transition.play();
-    }
-
-    private void slideSideGridOut(GridPane gridPane) {
-        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), gridPane);
-        transition.setToY(-(mainGrid.getTranslateY()));
-        transition.play();
-    }
-
-    private void slideBackButtonUp() {
+    private void slideBackButton(boolean slideOut) {
         TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), backButton);
-        transition.setToY(5);
+        if (slideOut) transition.setToY(100);
+        else transition.setToY(5);
         transition.play();
-    }
-
-    private void slideBackButtonDown() {
-        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.3), backButton);
-        transition.setToY(100);
-        transition.play();
-    }
-
-    @FXML
-    public void closeApplication() {
-        System.exit(0);
     }
 }
